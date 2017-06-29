218bb04d1865344ba81740b6487bcfeaacdb3d30
Sergey Plokhikh <sergey.plokhikh@iqoption.com>
2017-06-30 00:09:54 +0300

    simple working server with socket

--- src/github.com/server/main.go
+++ src/github.com/server/main.go
@@ -1,38 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	_ "fmt"
+	_ "net"
+	_ "os"
+	"flag"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+	"net/url"
 )
 
-func main() {
-	var _ = fmt.Printf
+var addr = flag.String("addr", "127.0.0.1:8080", "http service address")
+var upgrader = websocket.Upgrader{}
 
-	ln, err := net.Listen("tcp", "localhost:8080")
+func handle(response http.ResponseWriter, request *http.Request) {
+	conn, err := upgrader.Upgrade(response, request, nil)
+
 	if err != nil {
 		panic(err)
 	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go handleConnection(conn)
-	}
-}
-
-func handleConnection(connection net.Conn) {
-	str := make([]byte, 1024)
+	defer conn.Close()
 
 	for {
-		if _, err := connection.Read(str); err != nil {
-			panic(err)
-		}
-
-		if string(str[:4]) == "exit" {
-			if err := connection.Close(); err != nil {
+		if mt, message, err := conn.ReadMessage(); err != nil {
+				panic(err)
+		} else {
+			log.Printf("recv: %s", message)
+			if err = conn.WriteMessage(mt, message); err != nil {
 				panic(err)
 			}
 		}
 	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+}
+
+func main() {
+	upgrader.CheckOrigin = checkSameOrigin
+	http.HandleFunc("/echo", handle)
+	http.HandleFunc("/", home)
+	panic(http.ListenAndServe(*addr, nil))
+}
+
+func checkSameOrigin(r *http.Request) bool {
+	//warning! security vulnerable! temporary turn off origin checking
+	return true
+	origin := r.Header["Origin"]
+	if len(origin) == 0 {
+		return true
+	}
+	u, err := url.Parse(origin[0])
+	if err != nil {
+		return false
+	}
+	return u.Host == r.Host
 }
