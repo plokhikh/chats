a1ad13dc52ca801ff6009a3f253c722bdd1f6f9b
Sergey Plokhikh <sergey.plokhikh@iqoption.com>
2017-07-01 11:05:17 +0300

    stable chat with route all

--- src/github.com/server/main.go
+++ src/github.com/server/main.go
@@ -1,52 +1,116 @@
 package main
 
 import (
-	_ "fmt"
-	_ "net"
-	_ "os"
 	"flag"
-	"log"
 	"net/http"
 	"github.com/gorilla/websocket"
 	"net/url"
+	"log"
+	//"fmt"
 )
+
+type msgStruct struct {
+	UserId int
+	Content []byte
+}
+
+type userConnect struct {
+	UserId int
+	Output chan []byte //output channel for a user
+	Input chan []byte //input channel from user
+	Sid []byte      //session id
+}
 
 var addr = flag.String("addr", "127.0.0.1:8080", "http service address")
 var upgrader = websocket.Upgrader{}
+//массив структур с id пользователя и исходящим каналом к нему
+var userConnects = make([]userConnect, 0, 10)
+var count = 0
 
+/**
+ * регистрируем пользователя, выдавая коннект
+ */
+func register(userId int, input chan []byte) userConnect {
+	var userConnect userConnect
+	log.Printf("register user: %d", userId)
+
+	userConnect.Output = make(chan []byte, 1024)
+	userConnect.UserId = userId
+	userConnect.Input = input
+
+	userConnects = append(userConnects, userConnect)
+
+	return userConnect
+}
+
+/**
+ * роутим сообщение нужным пользователям
+ */
+func route(msg msgStruct) {
+	for _, userConnect := range userConnects {
+		if msg.UserId != userConnect.UserId {
+			userConnect.Output <- msg.Content
+		}
+	}
+}
+
+/**
+ * обработка сокетного соединения браузером
+ */
 func handle(response http.ResponseWriter, request *http.Request) {
 	conn, err := upgrader.Upgrade(response, request, nil)
-
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
-	defer conn.Close()
+
+	//где-то здесь должна быть аутентификация
+	count++
+	userId := count
+
+	//превращаем Reader в chan
+	userInput := make(chan []byte, 1024)
+	go func(ch chan []byte) {
+		for {
+			if _, message, err := conn.ReadMessage(); err == nil {
+				log.Printf("recv: %s", message)
+				ch <- message
+			} else {
+				log.Print(err)
+				conn.Close()
+				break
+			}
+		}
+	} (userInput)
+
+	userConnect := register(userId, userInput)
 
 	for {
-		if mt, message, err := conn.ReadMessage(); err != nil {
-				panic(err)
-		} else {
-			log.Printf("recv: %s", message)
-			if err = conn.WriteMessage(mt, message); err != nil {
-				panic(err)
+		select {
+		//если прочитали из сокета от юзера - роутим сообщение
+		case bytes := <- userConnect.Input:
+			msg := msgStruct{UserId: userId, Content: bytes}
+			log.Printf("user %d print: %s", msg.UserId, msg.Content)
+		//асинхронно рассылаем сообщение пользователям
+			go route(msg)
+		//если прочитали что-то из входящего канала юзеру - отправляем в сокет
+		case bytes := <- userConnect.Output:
+			log.Printf("send message \"%s\" for user %d", bytes, userConnect.UserId)
+			if err = conn.WriteMessage(websocket.TextMessage, bytes); err != nil {
+				log.Print(err)
+				conn.Close()
+				break
 			}
 		}
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-}
-
 func main() {
 	upgrader.CheckOrigin = checkSameOrigin
-	http.HandleFunc("/echo", handle)
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", handle)
 	panic(http.ListenAndServe(*addr, nil))
 }
 
 func checkSameOrigin(r *http.Request) bool {
-	//warning! security vulnerable! temporary turn off origin checking
-	return true
 	origin := r.Header["Origin"]
 	if len(origin) == 0 {
 		return true
@@ -55,5 +119,8 @@ func checkSameOrigin(r *http.Request) bool {
 	if err != nil {
 		return false
 	}
+	//log.Println(u.Host, r.Host)
+	//warning! security vulnerable! temporary turn off origin checking
+	return true
 	return u.Host == r.Host
 }
